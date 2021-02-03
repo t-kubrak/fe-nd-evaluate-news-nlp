acf15048847bfd1db66651065f2e542ce20ba61a
tkubrak <Pre@36670>
2021-02-03 14:47:58 +0200

    feat: send request for sentiment analysis api

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,21 @@
 function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let text = document.getElementById('name').value
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    fetch('http://localhost:8081/analyse', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({text}),
     })
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('results').innerHTML = JSON.stringify(res);
+        });
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 var path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mockAPIResponse = require('./mockAPI.js')
+const fetch = require('node-fetch');
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+
+const apiBaseUrl = 'https://api.meaningcloud.com/sentiment-2.1';
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -16,10 +27,26 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+app.post('/analyse', async function (req, res) {
+    const endpoint = `${apiBaseUrl}?key=${process.env.API_KEY}&lang=en&txt=${req.body.text}`;
+
+    const apiRes = await fetch(encodeURI(endpoint), {
+        method: 'POST',
+    });
+
+    try {
+        res.send(await apiRes.json());
+    } catch (error) {
+        console.log("error", error);
+    }
+})
+
+
