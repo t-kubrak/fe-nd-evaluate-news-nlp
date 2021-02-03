454f8863dfe99c59594114aa7b3cd381c0fafdf3
tkubrak <Pre@36670>
2021-02-03 14:46:33 +0200

    fix: form

--- src/client/index.js
+++ src/client/index.js
@@ -7,7 +7,7 @@ import './styles/form.scss'
 import './styles/footer.scss'
 import './styles/header.scss'
 
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+export {
+    checkForName,
+    handleSubmit
+}
